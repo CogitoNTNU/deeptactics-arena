ba97e529b2a310dc4e39b828f39edc66373216c6
Sverre Nystad <sverrenystad@gmail.com>
2026-03-12 18:11:46 +0100

    feat: simplify names

--- src/environment.py
+++ src/environment.py
@@ -4,11 +4,11 @@ from pettingzoo.classic import chess_v6, connect_four_v3, tictactoe_v3
 
 def build_environment(env_name: str) -> AECEnv:
     match env_name.lower():
-        case "chess_v6":
+        case "chess":
             return chess_v6.env()
-        case "connect_four_v3":
+        case "connect_four":
             return connect_four_v3.env()
-        case "tictactoe_v3":
+        case "tic_tac_toe":
             return tictactoe_v3.env()
         case _:
             raise ValueError(f"Unknown environment: {env_name}")
