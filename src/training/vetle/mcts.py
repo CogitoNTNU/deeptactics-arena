897b0d5bb8abe723d641f21cfec94f21c2629b95
Knolaisen <knolaise@gmail.com>
2026-04-07 18:20:50 +0200

    fix: add legal actions to dirlech noise

--- src/training/vetle/mcts.py
+++ src/training/vetle/mcts.py
@@ -7,37 +7,41 @@ from src.configuration import Configuration
 from copy import deepcopy
 from gymnasium import Env
 
-class MCTS():
-    def __init__(self, env: Env, config: Configuration, model):
+
+class MCTS:
+    def __init__(self, env: Env, config: Configuration, model, device: str = "cpu"):
         self.config = config
-        
+
         self.c_puct = self.config.mcts.cpuct
         self.pi_temp = self.config.mcts.pi_temp
-        self.inv_temp = 1/self.pi_temp
+        self.inv_temp = 1 / self.pi_temp
 
         self.env = env
         self.env.reset()
-        
+
         self.network = model
 
-        self.root = Node(self.network, env)
-        self.root.pred_pol = self.dirichlet(self.root.pred_pol, self.config.mcts.epsilon)
+        self.root = Node(self.network, env, device=device)
+        self.root.pred_pol = self.dirichlet(
+            self.root.pred_pol,
+            legal_actions=self.root.legal_actions,
+            epsilon=self.config.mcts.epsilon,
+        )
         self.num_root_actions = self.env.legal_moves()
-        
-    
+
     def backpropogate(self, node: Node, value: float) -> None:
         """Backpropagate value up tree"""
         while node.parent != None:
             node.value += value
             node.num_visited += 1
-            node.avg = node.value/node.num_visited
+            node.avg = node.value / node.num_visited
 
             value = -value
             node = node.parent
         node.value += value
         node.num_visited += 1
-        node.avg = node.value/node.num_visited
-    
+        node.avg = node.value / node.num_visited
+
     def PUCT(self, node: Node) -> float:
         """Calculate PUCT for a node and state"""
         PUCT_vals = []
@@ -45,84 +49,88 @@ class MCTS():
             if action in node.children:
                 val = node.children[action].avg
 
-                val += self.c_puct * node.pred_pol.tolist()[action] * node.num_visited**(0.5) / (1+node.children[action].num_visited)
+                val += (
+                    self.c_puct
+                    * node.pred_pol.tolist()[action]
+                    * node.num_visited ** (0.5)
+                    / (1 + node.children[action].num_visited)
+                )
 
                 PUCT_vals.append(val)
             else:
                 PUCT_vals.append(-1e15)
 
         return torch.argmax(torch.asarray(PUCT_vals)).item()
-    
+
     def policy(self, node: Node, action) -> float:
         """Calculate pi for given action"""
-        val = node.children[action].num_visited**(self.inv_temp)
-        val /= sum([node.children[i].num_visited**(self.inv_temp) for i in node.children])
-        
-        return val
-    
+        val = node.children[action].num_visited ** (self.inv_temp)
+        val /= sum(
+            [node.children[i].num_visited ** (self.inv_temp) for i in node.children]
+        )
 
-    def dirichlet(self, pred_pol, legal_actions, epsilon, alpha = 0.3):
+        return val
+
+    def dirichlet(self, pred_pol, legal_actions, epsilon, alpha=0.3):
         prior = pred_pol.clone()
         conc = torch.full(
-            (len(legal_actions),),alpha,
-            dtype = prior.dtype,
-            device = prior.device
+            (len(legal_actions),), alpha, dtype=prior.dtype, device=prior.device
         )
-        
-        noise = torch.distributions.Dirichlet(conc).sample()
-        #på bare legal actions
-        prior[legal_actions] = (1-epsilon) * prior[legal_actions] + epsilon * noise
 
-        #normaliserer
+        noise = torch.distributions.Dirichlet(conc).sample()
+        # på bare legal actions
+        prior[legal_actions] = (1 - epsilon) * prior[legal_actions] + epsilon * noise
+
+        # normaliserer
         prior = prior.clamp_min(0)
         prior = prior / prior.sum()
 
         return prior
 
-    
-
     def traverse(self, node: Node):
-        
+
         if len(node.children) != 0:
-            #print(f"{node.action}: finn neste child {node.children.keys()}")
+            # print(f"{node.action}: finn neste child {node.children.keys()}")
 
             best_node = node.children[self.PUCT(node)]
-            
+
             self.traverse(best_node)
 
         elif node.num_visited == 0:
-            #print(f"{node.action}: Ikke besøkt før, gjør rollout")
+            # print(f"{node.action}: Ikke besøkt før, gjør rollout")
             self.rollout(node)
-        else: 
-            
+        else:
+
             # mask = node.obs["action_mask"]
             # legal = [node.env.legal_moves()[i] for i in range(len(node.env.legal_moves())) if mask[i]]
-            #print(legal, node.terminated, node.truncated)
+            # print(legal, node.terminated, node.truncated)
             legal = node.env.legal_moves()
 
-            if (len(legal)==0) or node.truncated or node.terminated:
-                #print(f"{node.action}: Besøkt før: finn mulige actions og gjør en: spillet slutt")
+            if (len(legal) == 0) or node.truncated or node.terminated:
+                # print(f"{node.action}: Besøkt før: finn mulige actions og gjør en: spillet slutt")
                 self.backpropogate(node, node.reward)
-            
+
             else:
-                #print(f"{node.action}: Besøkt før: finn mulige actions og gjør en: Legg til barn")
+                # print(f"{node.action}: Besøkt før: finn mulige actions og gjør en: Legg til barn")
                 node.add_children(self.network)
                 self.traverse(node.children[legal[0]])
 
     def rollout(self, node: Node):
         self.backpropogate(node, node.pred_val.item())
 
-
     def run_simulations(self, num_simulations):
         for i in range(num_simulations):
-            #print(f"{i}----------------------------")
+            # print(f"{i}----------------------------")
             self.traverse(self.root)
- 
-        a = torch.asarray([0 if x not in self.root.children else self.policy(self.root, x) for x in self.num_root_actions], dtype=torch.float32)
-    
-        #print("Ferdig")
+
+        a = torch.asarray(
+            [
+                0 if x not in self.root.children else self.policy(self.root, x)
+                for x in self.num_root_actions
+            ],
+            dtype=torch.float32,
+        )
+
+        # print("Ferdig")
 
         return a
-        
-
-    
