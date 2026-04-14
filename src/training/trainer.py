7cce9effc7191302cc84c2975ecfea4e834b4557
Knolaisen <knolaise@gmail.com>
2026-04-14 19:42:49 +0200

    fix: add optimizer.step back

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -85,6 +85,7 @@ def train_one_epoch(
 
         grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
+        optimizer.step()
         scheduler.step()
 
         with torch.no_grad():
